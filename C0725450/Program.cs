9432ed7e1a482929654ef95a9a63852a0d3d809c
c0725450 <komalgillc3@gmail.com>
2019-03-14 13:33:33 -0400

    Commit 1:33

--- C0725450/Program.cs
+++ C0725450/Program.cs
@@ -10,6 +10,50 @@ namespace C0725450
     {
         static void Main(string[] args)
         {
+
         }
+    }
+
+    public class BankAccount
+    {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+        private BankAccount()
+        {
+
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account Frozen");
+            }
+            if (amount > m_balance){
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+        }
+        
+
     }
 }
